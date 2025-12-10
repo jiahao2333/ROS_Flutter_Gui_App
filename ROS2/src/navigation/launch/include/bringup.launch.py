eb4fc59283810c72af7e3191723d6cd4b28f01bb
佳昊 [陈 佳昊] <1198408384@qq.com>
2025-12-11 01:10:45 +0800

    Update bringup.launch.py

--- ROS2/src/navigation/launch/include/bringup.launch.py
+++ ROS2/src/navigation/launch/include/bringup.launch.py
@@ -19,10 +19,8 @@ def launch_setup(context):
         navigation_package_path = get_package_share_directory('navigation')
     else:
         navigation_package_path = '/home/ubuntu/ros2_ws/src/navigation'
-        controller_package_path = '/home/ubuntu/ros2_ws/src/driver/controller'
         app_package_path = '/home/ubuntu/ros2_ws/src/app'
         peripherals_package_path = '/home/ubuntu/ros2_ws/src/peripherals'
-
     # Create the launch configuration variables
     rtabmap = LaunchConfiguration('rtabmap')
     namespace = LaunchConfiguration('namespace')
@@ -40,21 +38,6 @@ def launch_setup(context):
     declare_rtabmap_cmd = DeclareLaunchArgument(
         'rtabmap',
         default_value='false',
-    )
-
-    depth_camera_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(peripherals_package_path, 'launch/depth_camera.launch.py')),
-    )
-    rosbridge_websocket_launch = ExecuteProcess(
-            cmd=['ros2', 'launch', 'rosbridge_server', 'rosbridge_websocket_launch.xml'],
-            output='screen'
-        )
-
-    web_video_server_node = Node(
-        package='web_video_server',
-        executable='web_video_server',
-        output='screen',
     )
 
     stdout_linebuf_envvar = SetEnvironmentVariable(
@@ -136,9 +119,6 @@ def launch_setup(context):
             declare_params_file_cmd, 
             declare_log_level_cmd,
             declare_use_teb_cmd,
-            depth_camera_launch,
-            rosbridge_websocket_launch,
-            web_video_server_node,
             bringup_cmd_group]
 
 def generate_launch_description():
