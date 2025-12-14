a19bffbc2125c43e5629a43e80771a8e92ac661a
佳昊 [陈 佳昊] <1198408384@qq.com>
2025-12-15 00:14:14 +0800

    Create gpio.py

---
+++ ROS2/gpio.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+# 初始化 LED 对象，参数为 BCM 编号
+led = LED(4)
+
+while True:
+    led.on()   # 点亮
+    print("LED On")
+    sleep(1)   # 等待1秒
+    led.off()  # 熄灭
+    print("LED Off")
+    sleep(1)
